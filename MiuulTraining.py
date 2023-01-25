d2a08efed4f0669cd5921e8104e17f3b4bd45d1a
Kokimen <77778536+Kokimen@users.noreply.github.com>
2023-01-25 21:22:33 +0300

    Miuul

--- MiuulTraining.py
+++ MiuulTraining.py
@@ -686,10 +686,10 @@ for salary in salaries:
     else:
         null_list.append(new_salary(salary * 2))
 
-#eğer if tek başına ise en sağda
-#eğer else var ise for en sağda, for'un solunda da else
+# eğer if tek başına ise en sağda
+# eğer else var ise for en sağda, for'un solunda da else
 [salary * 2 if salary > 300 else salary * 1.5 for salary in salaries]
-[new_salary(salary) if salary < 300 else salary*0.9 for salary in salaries]
+[new_salary(salary) if salary < 300 else salary * 0.9 for salary in salaries]
 
 students = ["jen", "jun", "jon", "jin"]
 students_no = ["jen", "jon"]
@@ -703,28 +703,29 @@ dictionary.keys()
 dictionary.values()
 dictionary.items()
 
-#hayat kurtaran! key ya da valueye özel bir şekilde müdahale etmek
-{k: v**2 for(k,v) in dictionary.items()}
-{k.upper(): v for (k,v) in dictionary.items()}
+# hayat kurtaran! key ya da valueye özel bir şekilde müdahale etmek
+{k: v ** 2 for (k, v) in dictionary.items()}
+{k.upper(): v for (k, v) in dictionary.items()}
 
-#Uygulama Mülakat Sorusu Dict#
-#çift sayıların karesi alınacak bir sözlüğe eklenecek
-#keyler orijinal kalacak, valueler ise değişecek
+# Uygulama Mülakat Sorusu Dict#
+# çift sayıların karesi alınacak bir sözlüğe eklenecek
+# keyler orijinal kalacak, valueler ise değişecek
 
 numbers = range(10)
 new_dict = {}
 
 for n in numbers:
     if n % 2 == 0:
-        #hayat kurtaran! elemanları gezilen numbers içerisindeki n'lere dokunmadık, aynı n ifadesinin karesini value
-        #bölümüne ekledik, n'i köşeli paranteze alarak değerini key bölümüne ekledik
-        new_dict[n] = n**2
+        # hayat kurtaran! elemanları gezilen numbers içerisindeki n'lere dokunmadık, aynı n ifadesinin karesini value
+        # bölümüne ekledik, n'i köşeli paranteze alarak değerini key bölümüne ekledik
+        new_dict[n] = n ** 2
 
-{n: n**2 for n in numbers if n % 2 == 0}
+{n: n ** 2 for n in numbers if n % 2 == 0}
 
 ##List & Dict Comprehension Uygulamaları# ÖNEMLİDİR
-#Bir Veri Steindeki Değişken İsimlerini Değiştirmek#
+# Bir Veri Steindeki Değişken İsimlerini Değiştirmek#
 import seaborn as sns
+
 df = sns.load_dataset("car_crashes")
 df.columns
 
@@ -738,11 +739,295 @@ df.columns = A
 
 df.columns = [col.upper() for col in df.columns]
 
-#İsminde INS olan değişkenlerin başına Flag olmayanlarınkine No_Flag eklemek
+# İsminde INS olan değişkenlerin başına Flag olmayanlarınkine No_Flag eklemek
 
 ["FLAG_" + col for col in df.columns if "INS" in col]
 
 ["FLAG_" + col if "INS" in col else "NO_FLAG_" + col for col in df.columns]
+
+# Amaç key'i string, values, aşağıdaki gibi bir liste olan sözlük oluşturmak, sadece sayısal değişkenler için yapılacak
+# Output
+# {{’total’: [’mean’, ‘min’, ‘max’, ‘var’],
+# ‘speeding’: [’mean’, ‘min’, ‘max’, ‘var’],
+# ‘alcohol’: [’mean’, ‘min’, ‘max’, ‘var’],
+# ‘not_distracted’: [’mean’, ‘min’, ‘max’, ‘var’],
+# ‘no_previous’: [’mean’, ‘min’, ‘max’, ‘var’],
+# ‘ins_premium’: [’mean’, ‘min’, ‘max’, ‘var’],
+# ‘ins_losses’: [’mean’, ‘min’, ‘max’, ‘var’]}
+
+import seaborn as sns
+
+df = sns.load_dataset('car_crashes')
+df.columns
+
+# tipi object olmayanları seç, "O" ifadesi objectin yanı stringin temsili.
+num_cols = [col for col in df.columns if df[col].dtype != "O"]
+soz = {}
+agg_list = ["mean", "min", "max", "sum"]
+
+for col in num_cols:
+    soz[col] = agg_list
+
+new_dict = {col: agg_list for col in num_cols}
+
+# hayat kurtaran!: [] içerisinde verilen değişkenler, dataframe içerisinden seçilir.
+df[num_cols].head()
+
+# hayat kurtaran!: agg fonksiyonu bir df'ye uygulanırsa ve içerisine sözlük gönderilirse, gönderilen sözlüğün içerisindeki
+# değişkenler df'de mevcut ise bu değişkenleri key bölümünde tutar. Value bölümüne girilen bütün değişkenleri de key'lerin
+# yanına getirir ve otomatik uygular.
+df[num_cols].agg(new_dict)
+
+###PYTHON VERİ ANALİZİ###
+# Numpy: matematiksel işlemler
+# Pandas: veri denilince akla gelen
+# Veri Görselleştirme: Matplotlib (low) & Seaborn (high)
+# Gelişmiş Fonksiyonel Keşifçi Veri Analizi: Büyük ölçekli projelerin fonksiyonel seviyede gerçekleştirilmesi
+
+# NUMPY#: Listelere göre daha verimli veri saklar, yüksek seviyede vektörel işlemler. Daha hızlı ve sabit tipte veri tutar.
+
+import numpy as np
+
+a = [1, 2, 3, 4]
+b = [2, 3, 4, 5]
+ab = []
+for i in range(0, len(a)):
+    ab.append(a[i] * b[i])
+
+# numpy hali
+a = np.array([1, 2, 3, 4])
+b = np.array([2, 3, 4, 5])
+a * b
+
+# numpy array'i oluşturmak
+np.array([1, 2, 3, 4, 5])
+type(np.array([1, 2, 3, 4, 5]))
+
+# 10 adet sıfırdan oluşan int liste
+np.zeros(10, dtype = int)
+
+# 0-10 arasında, 10 random int liste
+np.random.randint(0, 10, size = 10)
+
+# ortalaması 10, standart sapması 4 olan, 3x4 lük sayı litesi
+np.random.normal(10, 4, (3, 4))
+
+# numpy array özellikleri
+# ndim: boyut sayısı
+# shape: boyut bilgisi
+# size: toplam eleman sayısı
+# dtype: array veri tipi
+
+a = np.random.randint(10, size = 5)
+a.ndim
+a.shape
+a.size
+a.dtype
+
+# yeniden şekillendirme
+np.random.randint(1, 10, size = 9)
+np.random.randint(1, 10, size = 9).reshape(3, 3)
+
+ar = np.random.randint(1, 10, size = 9)
+ar.reshape(3, 3)
+
+# Index seçimi
+a = np.random.randint(10, size = 10)
+a[0]
+a[0:5]
+a[0] = 999
+a = np.random.randint(10, size = (3, 5))
+a[0, 0]  # sol taraf satır, sağ taraf sütun
+a[2, 3] = 2.9  # float değer olmaz, 2 değeri girilir, numpy tek tip veri tutar
+a[:, 1]  # bütün satırlar ve indexi sıfır olan sütunlar
+
+# Fancy Index
+v = np.arange(0, 30, 3)  # 0'dan 30'a 3'er artan array
+v[1]
+catch = [1, 2, 3]  # index numaraları
+v[catch]
+
+# Numpy Koşullu İşlemler
+v = np.array([1, 2, 3, 4, 5])
+ab = []
+for i in v:
+    if i < 3:
+        ab.append(i)
+# numpy hali
+v[v < 3]
+
+# Matematiksel İşlemler
+v = np.array([1, 2, 3, 4, 5])
+v / 5
+v * 5 / 10
+v ** 2
+v - 1
+
+np.subtract(v, 1)
+np.add(v, 1)
+np.mean(v)
+np.sum(v)
+np.min(v)
+np.max(v)
+np.var(v)
+
+# Numpy ile iki bilinmeyenli denklem çözümü
+# 5 * x0 + x1 = 12
+# x0 + 3 * x1 = 10
+
+a = np.array([[5, 1], [1, 3]])  # katsayılar
+b = np.array([12, 10])  # sonuçlar
+
+# linalg.solve: bilinmeyen değerleri hesaplar
+np.linalg.solve(a, b)
+
+######PYTHON PANDAS KÜTÜPHANESİ VERİ ANALİZİ########
+# VERİ OKUMA#
+import pandas as pd
+
+df = pd.read_csv("datasets/advertising.csv")
+df.head()
+
+# VERİYE HIZLI BAKIŞ#
+import pandas as pd
+import seaborn as sns
+
+df = sns.load_dataset("titanic")
+df.head()
+df.info
+df.columns
+df.index
+# betimsel özet bilgi, T transpose
+df.describe().T
+# değişkenlerden biri null mu
+df.isnull().values.any()
+# değişkenlerde eksik bilgi var mı
+df.isnull().sum()
+df["sex"].head()
+# hangi değişkende kaç sınıf var
+df["sex"].value_counts()
+
+# Pandasta Seçim İşlemleri
+import pandas as pd
+import seaborn as sns
+
+df = sns.load_dataset("titanic")
+df.head()
+
+# belirli bir aralıkta seçim
+df[0:13]
+# drop veri siler
+df.drop(0, axis = 0).head()
+delete_indexes = [1, 3, 5, 7]
+df.drop(delete_indexes, axis = 0).head(6)
+# işlemi kalıcı yapmak inplace
+df.drop(delete_indexes, axis = 0, inplace = True)
+
+# Değişkeni indexe çevirmek#
+df["age"].head()
+df.age.head()
+df.index = df["age"]
+df.drop("age", axis = 1, inplace = True)
+
+# Indexi değişkene çevirmek#
+df["age"] = df.index
+df.head()
+df = df.reset_index()
+
+# Değişkenlerle oynamak#
+import pandas as pd
+import seaborn as sns
+
+# bütün çıktıları göster
+pd.set_option('display.max_columns', None)
+df['age'].head()
+type(df['age'].head())
+df[['age']].head()
+type(df[['age']].head())
+
+# birden fazla değişkeni seçmek
+df[['age', 'alive']]
+col_names = ['age', 'adult_male', 'alive']
+df[col_names]
+
+df['age'] = df['age'] ** 2
+df['age3'] = df['age'] / df['age']
+df.drop('age3', axis = 1).head()
+# axis = 1 sütun sil demek
+df.drop(col_names, axis = 1).head()
+
+# LOC & ILOC
+# contains: stringleri ara, tilda işareti: olmayanları anlamına gelir. loc df'lerde seçme işlemleri için kullanılan özel yapı.
+df.loc[:, ~df.columns.str.contains('age')].head()
+
+# iloc: integer based, yani index bilgisi vererek seçim yapar
+# loc: label based yani indexlerde ki labellara göre seçim yapar
+
+# integer based seçim
+df.iloc[0:3]  # 3'e kadar seçer.
+
+# label based seçim
+df.loc[0:3]  # 3 dahil seçer.
+
+df.iloc[0:3, 0:3]
+df.loc[0:3, "age"]
+
+col_names = ["age", "embarked", "alive"]
+df.loc[0:3, col_names]
+
+# KOŞULLU SEÇİM#
+df[df["age"] > 50].head()
+df[df["age"] > 50].count()
+
+df.loc[df["age"] > 50, ["age", "class"]].head()
+
+df.loc[(df["age"] > 50) & (df["sex"] == "male"), ["age", "class"]].head()
+
+df_new = df.loc[(df["age"] > 50)
+                & (df["sex"] == "male")
+                & (df["embark_town"] == "southampton")
+                | (df["embark_town"] == "Cherbourg"),
+                ["age", "class", "embark_town"]].head()
+
+df_new["embark_town"].value_counts()
+
+#Toplulaştırma ve Gruplama (Aggregation and Grouping)#
+#count, first, last, mean, median, min, max, std, var, sum#
+
+df.groupby("sex")["age"].mean()
+
+df.groupby("sex").agg({"age": "mean"})
+
+df.groupby("sex").agg({"age": ["mean", "sum"]})
+
+df.groupby("sex").agg({"age": ["mean", "sum"], "survived": "mean"})
+
+df.groupby(["sex", "embark_town"]).agg({"age": ["mean", "sum"], "survived": "mean"})
+
+df.groupby(["sex", "embark_town"]).agg({"age": ["mean", "sum"], "survived": "mean", "sex": "count"})
+
+#Pivot Table#
+df.pivot_table("survived", "sex", "embarked")
+df.pivot_table("survived", "sex", ["embarked", "class"])
+
+df["new_age"] = pd.cut(df["age"], [0, 10, 20, 40, 70]) #sayısal değişkeni kategorik değişkene çevirir, değişkeni tanıyorsanız
+#cut, tanımıyorsanız çeyreğe bölsün diye qcut.
+
+df.pivot_table("survived", "sex", ["new_age", "class"])
+pd.set_option("display.width", 500)
+
+#Apply ve Lambda# satırlarda/sütunlarda elimizdeki fonksiyonu uygulamamızı sağlar / geçici olarak kullan at fonksiyon kullanmamızı sağlar
+import pandas as pd
+import seaborn as sns
+pd.set_option("display.max_columns", None)
+pd.set_option("display.width", 500)
+df = sns.load_dataset("titanic")
+df.head()
+
+df["age2"] = df["age"]*2
+df["age3"] = df["age"]*5
+
+(df["age"]/10).head()
 
 ##############################
 # BİRLEŞTİRME İŞLEMLERİ (JOIN)#
