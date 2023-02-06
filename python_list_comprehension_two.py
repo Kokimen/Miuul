509c49408d50e5f9ab1bac11435ace3f67542413
Kokimen <77778536+Kokimen@users.noreply.github.com>
2023-02-06 22:29:56 +0300

    python_list_comprehension_two.py

--- python_list_comprehension_two.py
+++ python_list_comprehension_two.py
@@ -1,6 +1,16 @@
 import seaborn as sns
 dfcc = sns.load_dataset("car_crashes")
 
+# ONE#
+["NUM_" + col.upper() if str(dfcc[col].dtypes) not in "object" else col.upper() for col in dfcc]
+
+# TWO#
+[col + "_FLAG" if "no" not in col else col for col in dfcc]
+
+# THREE#
+import seaborn as sns
+dfcc = sns.load_dataset("car_crashes")
+
 og_list = ["abbrev", "no_previous"]
 
 new_cols = [col for col in dfcc.columns if col not in og_list]
