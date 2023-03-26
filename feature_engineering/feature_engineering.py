c0995f211d3186dcbd1dbdf0d240b602851c9b49
Kokimen <77778536+Kokimen@users.noreply.github.com>
2023-03-26 15:55:39 +0300

    hard_again

--- feature_engineering/feature_engineering.py
+++ feature_engineering/feature_engineering.py
@@ -177,3 +177,100 @@ check_outlier(df, "Age")
 grab_outliers(df, "Age", index = True)
 remove_outliers(df, "Age")
 replace_with_threshold(df, "Age")
+
+# Local Outlier Factor
+df = sns.load_dataset("diamonds")
+df = df.select_dtypes(include = ["float64", "int64"])
+df = df.dropna()
+df.head()
+
+for col in df.columns:
+    print(col, check_outlier(df, col))
+
+low, up = outlier_thresholds(df, "depth")
+
+df[((df.depth < low) | (df.depth > up))].shape
+
+clf = LocalOutlierFactor(n_neighbors = 20)
+clf.fit_predict(df)
+df_scores = clf.negative_outlier_factor_
+
+scores = pd.DataFrame(np.sort(df_scores))
+scores.plot(stacked = True, xlim = [0, 20], style = ".-")
+plt.show()
+
+th = np.sort(df_scores)[3]
+
+df[df_scores < th].drop(axis = 0, labels = df[df_scores < th].index)
+
+# Missing Values
+# Catch the Missing Values
+df = load_titanic()
+# missing values numbers by columns
+df.isnull().sum()
+# total missing values numbers
+df.isnull().sum().sum()
+# show the at least one missing value
+df[df.isnull().any(axis = 1)]
+# show the no missing value
+df[df.notnull().all(axis = 1)]
+# show the missing values percentage
+(df.isnull().sum() / df.shape[0] * 100)
+# show the columns which contains missing values
+na_columns = [col for col in df.columns if df[col].isnull().sum() > 0]
+
+
+# Check the missing values specs
+def missing_values_table(dataframe, na_name=False):
+    na_columns = [col for col in df.columns if df[col].isnull().sum() > 0]
+    n_miss = dataframe[na_columns].isnull().sum().sort_values(ascending = False)
+    ratio = (dataframe[na_columns].isnull().sum() / dataframe.shape[0] * 100).sort_values(ascending = False)
+    missing_df = pd.concat([n_miss, np.round(ratio, 2)], axis = 1, keys = ["n_miss", "ratio"])
+    print(missing_df, end = "\n")
+    if na_name:
+        return na_columns
+
+
+missing_values_table(df)
+
+# Solve the Missing Values Problem
+# Remove
+df.dropna()
+# Fill with basic assignments
+df.Age.fillna(df.Age.mean())
+df.Age.fillna(df.Age.median())
+df.Age.fillna(0)
+# Only fill numerical columns
+df = df.apply(lambda x: x.fillna(x.mean()) if x.dtype != "O" else x, axis = 0)
+# Only fill categoric columns
+df.Embarked.fillna(df.Embarked.mode()[0])
+df.Embarked.fillna("missing")
+df = df.apply(lambda x: x.fillna(x.mode()[0]) if (x.dtype == "O" and len(x.unique()) <= 10) else x, axis = 0)
+# Assignment with categorical breakdown
+df.groupby("Sex")["Age"].mean()
+df.Age.fillna(df.groupby("Sex")["Age"].transform("mean")).isnull().sum()
+
+df.loc[(df.Age.isnull()) & (df.Sex == "female"), "Age"] = df.groupby("Sex")["Age"].mean()["female"]
+df.loc[(df.Age.isnull()) & (df.Sex == "male"), "Age"] = df.groupby("Sex")["Age"].mean()["male"]
+
+# This part is about machnine learning
+# Predictive Assignment
+categoric_cols, numeric_cols, categoric_but_cardinal = grab_col_names(df)
+numeric_cols = [col for col in numeric_cols if col not in "PassengerId"]
+dff = pd.get_dummies(df[categoric_cols + numeric_cols], drop_first = True)
+
+# Standardization of Variables
+scaler = MinMaxScaler()
+dff = pd.DataFrame(scaler.fit_transform(dff), columns = dff.columns)
+
+# Application of KNN
+from sklearn.impute import KNNImputer
+
+imputer = KNNImputer(n_neighbors = 5)
+dff = pd.DataFrame(imputer.fit_transform(dff), columns = dff.columns)
+
+# Data rollback to make sense of it
+dff = pd.DataFrame(scaler.inverse_transform(dff), columns = dff.columns)
+
+df["age_imputed_knn"] = dff[["Age"]]
+df.loc[df.Age.isnull(), ["Age", "age_imputed_knn"]]
