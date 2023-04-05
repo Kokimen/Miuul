ed29f45d9942b08ad3b05b5120780384a98020a4
Kokimen <77778536+Kokimen@users.noreply.github.com>
2023-04-05 22:31:25 +0300

    hard_again

--- feature_engineering/feature_engineering_case_study_II.py
+++ feature_engineering/feature_engineering_case_study_II.py
@@ -1,14 +1,9 @@
-import itertools
 import numpy as np
 import pandas as pd
 import seaborn as sns
-import math
-from datetime import date
-import statsmodels.stats.api as sms
+
 from matplotlib import pyplot as plt
-from scipy.stats import shapiro, levene, ttest_ind, mannwhitneyu
-from statsmodels.stats.proportion import proportions_ztest
-import missingno as msno
+
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import LocalOutlierFactor
@@ -192,6 +187,7 @@ df.loc[(df["BMI"] >= 40.0), "New_BMI"] = "Obesite_3"
 df.loc[(df["Glucose"] >= 80) & (df["Glucose"] <= 100), "New_Glucose"] = "Normal"
 df.loc[(df["Glucose"] >= 101) & (df["Glucose"] <= 125), "New_Glucose"] = "Impaired-Glucose"
 df.loc[(df["Glucose"] >= 126), "New_Glucose"] = "Diabetic"
+
 
 # df["BMI_Skin"] = df["BMI"] * df["SkinThickness"]
 
