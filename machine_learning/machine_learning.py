e2878dce53c899894ba04ed9415ad75b8f5cafa7
Kokimen <77778536+Kokimen@users.noreply.github.com>
2023-04-08 16:57:38 +0300

    ML_confusing

--- machine_learning/machine_learning.py
+++ machine_learning/machine_learning.py
@@ -99,4 +99,67 @@ reg_model.score(X_test, y_test)
 # 10 Katlı Cross Validation - CV RMSE
 np.mean(np.sqrt(-cross_val_score(reg_model, X, y, cv = 10, scoring = "neg_mean_squared_error")))
 
+
 # Simple Linear Regression with Gradient Descent from Stratch
+# Cost function MSE
+def cost(Y, b, w, X):
+    m = len(Y)
+    sse = 0
+
+    for i in range(0, m):
+        y_hat = b + w * X[i]
+        y = Y[i]
+        sse += (y_hat - y) ** 2
+
+    mse = sse / m
+    return mse
+
+
+# update_weights
+def update_weights(Y, b, w, X, learning_rate):
+    m = len(Y)
+    b_deriv_sum = 0
+    w_deriv_sum = 0
+    for i in range(0, m):
+        y_hat = b + w * X[i]
+        y = Y[i]
+        b_deriv_sum += (y_hat - y)
+        w_deriv_sum += (y_hat - y) * X[i]
+    new_b = b - (learning_rate * 1 / m * b_deriv_sum)
+    new_w = w - (learning_rate * 1 / m * w_deriv_sum)
+    return new_b, new_w
+
+
+# train fonksiyonu
+def train(Y, initial_b, initial_w, X, learning_rate, num_iters):
+    print("Starting gradient descent at b = {0}, w = {1}, mse = {2}".format(initial_b, initial_w,
+                                                                            cost(Y, initial_b, initial_w, X)))
+
+    b = initial_b
+    w = initial_w
+    cost_history = []
+
+    for i in range(num_iters):
+        b, w = update_weights(Y, b, w, X, learning_rate)
+        mse = cost(Y, b, w, X)
+        cost_history.append(mse)
+
+        if i % 100 == 0:
+            print("iter={:d}    b={:.2f}    w={:.4f}    mse={:.4}".format(i, b, w, mse))
+
+    print("After {0} iterations b = {1}, w = {2}, mse = {3}".format(num_iters, b, w, cost(Y, b, w, X)))
+    return cost_history, b, w
+
+
+df = pd.read_csv("datasets/kaggle/advertising.csv")
+
+X = df["radio"]
+Y = df["sales"]
+
+# Hyperparameters
+learning_rate = .001
+initial_b = .001
+initial_w = .001
+num_iters = 10000
+
+cost_history, b, w = train(Y, initial_b, initial_w, X, learning_rate, num_iters)
