a14539414927fdc56f759e8e744395cac78afcc2
Kokimen <77778536+Kokimen@users.noreply.github.com>
2023-04-08 19:16:24 +0300

    ML_confusing

---
+++ machine_learning/salary_predict_case_study_I.py
@@ -0,0 +1,74 @@
+﻿import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+from sklearn.model_selection import train_test_split, cross_val_score
+
+from machine_learning.machine_learning import reg_model
+
+
+def pd_options():
+    pd.set_option("display.max_columns", 35)
+    pd.set_option("display.max_rows", 35)
+    pd.set_option('display.width', 300)
+    pd.set_option("display.expand_frame_repr", True)
+    pd.options.mode.chained_assignment = None
+    pd.set_option("display.float_format", lambda x: "%.3f" % x)
+
+
+pd_options()
+
+df = pd.read_excel("datasets/miuul/linear_reg_salary_predict.xlsx")
+
+df = df.set_axis(["Experience", "Salary"], axis = "columns")
+# Linear Regression
+# bias = 275, weight = 90 (y=b+wx)
+X = df[["Experience"]]
+y = df[["Salary"]]
+
+# Visualization
+plt.figure(figsize = (12, 6))
+sns.pairplot(df, x_vars = ['Experience'], y_vars = ['Salary'], size = 7, kind = 'scatter')
+plt.xlabel('Years')
+plt.ylabel('Salary')
+plt.title('Salary Prediction')
+plt.show()
+
+reg_model = LinearRegression().fit(X, y)
+
+# sabit (b - bias)
+reg_model.intercept_
+
+# TV'nin katsayısı (w1)
+reg_model.coef_[0]
+
+# Prediction
+reg_model.intercept_ + reg_model.coef_[0]
+
+# Prediction Success
+# MSE
+y_pred = reg_model.predict(X)
+mean_squared_error(y, y_pred)
+y.mean(), y.std()
+
+# RMSE
+np.sqrt(mean_squared_error(y, y_pred))
+
+# MAE
+mean_absolute_error(y, y_pred)
+
+# R-Square
+reg_model.score(X, y)
+
+# Visualization
+g = sns.regplot(x = X, y = y, scatter_kws = {"color": "b", "s": 15}, ci = False, color = "r")
+
+g.set_title(f"Moden Denklemi: Salary = {round(reg_model.intercept_[0], 2)}+Experience*{round(reg_model.coef_[0][0], 2)}")
+g.set_ylabel("Maaş")
+g.set_xlabel("Deneyim")
+plt.xlim(1, 12)
+plt.ylim(bottom = 0)
+plt.show()
