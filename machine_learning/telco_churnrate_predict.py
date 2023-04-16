d648a65a86a65954de3b34e7432bbbac42ba4d02
Kokimen <77778536+Kokimen@users.noreply.github.com>
2023-04-16 17:38:28 +0300

    ML_confusing

--- machine_learning/telco_churnrate_predict.py
+++ machine_learning/telco_churnrate_predict.py
@@ -14,11 +14,13 @@ import pandas as pd
 import seaborn as sns
 from matplotlib import pyplot as plt
 import missingno as msno
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, confusion_matrix
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import LocalOutlierFactor
 from sklearn.preprocessing import LabelEncoder, StandardScaler, RobustScaler
 from sklearn.metrics import classification_report
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
 
 
 def pd_options():
@@ -123,12 +125,105 @@ def outlier_thresholds(dataframe, variable):
 def check_outlier(dataframe, col_name):
     low_limit, up_limit = outlier_thresholds(dataframe, col_name)
     if dataframe[dataframe[col_name] > up_limit].any(axis = None):
-        return (f"--> Uplimit Outlier, {low_limit, up_limit}")
+        return f"--> Uplimit Outlier, {low_limit, up_limit}"
     elif dataframe[dataframe[col_name] < low_limit].any(axis = None):
-        return (f" --> Lowlimit Outlier, {low_limit, up_limit}")
+        return f" --> Lowlimit Outlier, {low_limit, up_limit}"
     else:
-        return (f" --> No Outlier, {low_limit, up_limit}")
+        return f" --> No Outlier, {low_limit, up_limit}"
 
 
 for col in numeric_cols:
     print(col, check_outlier(df, col))
+
+
+# Encoding
+def label_encoder(dataframe, binary_col):
+    labelencoder = LabelEncoder()
+    dataframe[binary_col] = labelencoder.fit_transform(dataframe[binary_col])
+    return dataframe
+
+
+binary_cols = [col for col in df.columns if df[col].dtype not in [int, float]
+               and df[col].nunique() == 2]
+
+for col in binary_cols:
+    df = label_encoder(df, col)
+
+
+# Rare Analyser and Rare Encoding
+def rare_analyser(dataframe, target, categoric_cols):
+    for col in categoric_cols:
+        print(col, ":", len(dataframe[col].value_counts()))
+        print(pd.DataFrame({"COUNT": dataframe[col].value_counts(),
+                            "RATIO": dataframe[col].value_counts() / len(dataframe),
+                            "TARGET_MEAN": dataframe.groupby(col)[target].mean()}), end = "\n\n\n")
+
+
+rare_analyser(df, "Churn", categoric_cols)
+
+
+def rare_encoder(dataframe, rare_percentage):
+    temp_df = dataframe.copy()
+    rare_columns = [col for col in temp_df.columns if temp_df[col].dtypes == "O"
+                    and (temp_df[col].value_counts() / len(temp_df) < rare_percentage).any(axis = None)]
+
+    for var in rare_columns:
+        tmp = temp_df[var].value_counts() / len(temp_df)
+        rare_labels = tmp[tmp < rare_percentage].index
+        temp_df[var] = np.where(temp_df[var].isin(rare_labels), "Rare", temp_df[var])
+
+    return temp_df
+
+
+rare_encoder(df, .01)
+
+
+# One Hot Encoding
+def one_hot_encoder(dataframe, categorical_cols, drop_first=False):
+    dataframe = pd.get_dummies(dataframe, columns = categorical_cols, drop_first = drop_first)
+    return dataframe
+
+
+ohe_cols = [col for col in df.columns if 10 >= df[col].nunique() > 2]
+
+df = one_hot_encoder(df, ohe_cols, drop_first = True)
+
+# Normalization
+scaler = StandardScaler()
+df[numeric_cols] = scaler.fit_transform(df[numeric_cols])
+
+
+# Importance of Features with Visualization Function
+def plot_importance(model, features, num=len(X)):
+    feature_imp = pd.DataFrame({'Value': model.feature_importances_, 'Feature': features.columns})
+    plt.figure(figsize = (10, 10))
+    sns.set(font_scale = 1)
+    sns.barplot(x = "Value", y = "Feature", data = feature_imp.sort_values(by = "Value",
+                                                                           ascending = False)[0:num])
+    plt.title('Features')
+    plt.tight_layout()
+    plt.show()
+
+
+# Creating Different Models and Test
+y = df["Churn"]  # --> dependent variable
+X = df.drop(["Churn"], axis = 1)  # --> other variables are independent variables
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = .2, random_state = 17)
+###############
+# Random Forest
+###############
+rf_model = RandomForestClassifier(random_state = 46).fit(X_train, y_train)
+y_pred = rf_model.predict(X_test)
+print(classification_report(y_pred, y_test))
+# Accuracy: .79
+
+###############
+# Decision Tree
+###############
+cart_model = DecisionTreeClassifier(random_state = 46).fit(X_train, y_train)
+y_pred = cart_model.predict(X_test)
+print(classification_report(y_pred, y_test))
+# Accuracy: .71
+
+
+plot_importance(rf_model, X_train)
