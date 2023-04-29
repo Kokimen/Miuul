8ca3d5795f86e2f2deb855263e2f602f36c9ea16
Kokimen <77778536+Kokimen@users.noreply.github.com>
2023-04-29 23:48:38 +0300

    ML_confusing

--- machine_learning/unsupervised_learning.py
+++ machine_learning/unsupervised_learning.py
@@ -174,3 +174,84 @@ pca_fit = pca.fit_transform(df)
 
 pca.explained_variance_ratio_
 np.cumsum(pca.explained_variance_ratio_)
+
+################################
+# Optimum Bileşen Sayısı
+################################
+
+pca = PCA().fit(df)
+plt.plot(np.cumsum(pca.explained_variance_ratio_))
+plt.xlabel("Bileşen Sayısını")
+plt.ylabel("Kümülatif Varyans Oranı")
+plt.show()
+
+################################
+# Final PCA'in Oluşturulması
+################################
+
+pca = PCA(n_components = 3)
+pca_fit = pca.fit_transform(df)
+
+pca.explained_variance_ratio_
+np.cumsum(pca.explained_variance_ratio_)
+
+################################
+# BONUS: Principal Component Regression
+################################
+
+df = pd.read_csv("datasets/Hitters.csv")
+df.shape
+
+len(pca_fit)
+
+num_cols = [col for col in df.columns if df[col].dtypes != "O" and "Salary" not in col]
+len(num_cols)
+
+others = [col for col in df.columns if col not in num_cols]
+
+pd.DataFrame(pca_fit, columns = ["PC1", "PC2", "PC3"]).head()
+
+df[others].head()
+
+final_df = pd.concat([pd.DataFrame(pca_fit, columns = ["PC1", "PC2", "PC3"]),
+                      df[others]], axis = 1)
+final_df.head()
+
+from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeRegressor
+
+
+def label_encoder(dataframe, binary_col):
+    labelencoder = LabelEncoder()
+    dataframe[binary_col] = labelencoder.fit_transform(dataframe[binary_col])
+    return dataframe
+
+
+for col in ["NewLeague", "Division", "League"]:
+    label_encoder(final_df, col)
+
+final_df.dropna(inplace = True)
+
+y = final_df["Salary"]
+X = final_df.drop(["Salary"], axis = 1)
+
+lm = LinearRegression()
+rmse = np.mean(np.sqrt(-cross_val_score(lm, X, y, cv = 5, scoring = "neg_mean_squared_error")))
+y.mean()
+
+cart = DecisionTreeRegressor()
+rmse = np.mean(np.sqrt(-cross_val_score(cart, X, y, cv = 5, scoring = "neg_mean_squared_error")))
+
+cart_params = {'max_depth': range(1, 11),
+               "min_samples_split": range(2, 20)}
+
+# GridSearchCV
+cart_best_grid = GridSearchCV(cart,
+                              cart_params,
+                              cv = 5,
+                              n_jobs = -1,
+                              verbose = True).fit(X, y)
+
+cart_final = DecisionTreeRegressor(**cart_best_grid.best_params_, random_state = 17).fit(X, y)
+
+rmse = np.mean(np.sqrt(-cross_val_score(cart_final, X, y, cv = 5, scoring = "neg_mean_squared_error")))
