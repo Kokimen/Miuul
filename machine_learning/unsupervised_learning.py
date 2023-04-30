3efcb00da46ea2e3fa7b6c39c056256924ddee38
Kokimen <77778536+Kokimen@users.noreply.github.com>
2023-04-30 23:55:06 +0300

    ML_confusing

--- machine_learning/unsupervised_learning.py
+++ machine_learning/unsupervised_learning.py
@@ -255,3 +255,80 @@ cart_best_grid = GridSearchCV(cart,
 cart_final = DecisionTreeRegressor(**cart_best_grid.best_params_, random_state = 17).fit(X, y)
 
 rmse = np.mean(np.sqrt(-cross_val_score(cart_final, X, y, cv = 5, scoring = "neg_mean_squared_error")))
+
+################################
+# BONUS: PCA ile Çok Boyutlu Veriyi 2 Boyutta Görselleştirme
+################################
+
+################################
+# Breast Cancer
+################################
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 500)
+
+df = pd.read_csv("datasets/breast_cancer.csv")
+
+y = df["diagnosis"]
+X = df.drop(["diagnosis", "id"], axis = 1)
+
+
+def create_pca_df(X, y):
+    X = StandardScaler().fit_transform(X)
+    pca = PCA(n_components = 2)
+    pca_fit = pca.fit_transform(X)
+    pca_df = pd.DataFrame(data = pca_fit, columns = ['PC1', 'PC2'])
+    final_df = pd.concat([pca_df, pd.DataFrame(y)], axis = 1)
+    return final_df
+
+
+pca_df = create_pca_df(X, y)
+
+
+def plot_pca(dataframe, target):
+    fig = plt.figure(figsize = (7, 5))
+    ax = fig.add_subplot(1, 1, 1)
+    ax.set_xlabel('PC1', fontsize = 15)
+    ax.set_ylabel('PC2', fontsize = 15)
+    ax.set_title(f'{target.capitalize()} ', fontsize = 20)
+
+    targets = list(dataframe[target].unique())
+    colors = random.sample(['r', 'b', "g", "y"], len(targets))
+
+    for t, color in zip(targets, colors):
+        indices = dataframe[target] == t
+        ax.scatter(dataframe.loc[indices, 'PC1'], dataframe.loc[indices, 'PC2'], c = color, s = 50)
+    ax.legend(targets)
+    ax.grid()
+    plt.show()
+
+
+plot_pca(pca_df, "diagnosis")
+
+################################
+# Iris
+################################
+
+import seaborn as sns
+
+df = sns.load_dataset("iris")
+
+y = df["species"]
+X = df.drop(["species"], axis = 1)
+
+pca_df = create_pca_df(X, y)
+
+plot_pca(pca_df, "species")
+
+################################
+# Diabetes
+################################
+
+df = pd.read_csv("datasets/diabetes.csv")
+
+y = df["Outcome"]
+X = df.drop(["Outcome"], axis = 1)
+
+pca_df = create_pca_df(X, y)
+
+plot_pca(pca_df, "Outcome")
