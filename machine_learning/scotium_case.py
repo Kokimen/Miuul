64f6419fb43ea64cb6471bbb4b2163eb5509c4fe
Kokimen <77778536+Kokimen@users.noreply.github.com>
2023-05-06 23:04:51 +0300

    ML_confusing

--- machine_learning/scotium_case.py
+++ machine_learning/scotium_case.py
@@ -11,3 +11,15 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 from sklearn.model_selection import cross_val_score, GridSearchCV
 from sklearn.preprocessing import LabelEncoder
+
+
+def pd_options():
+    pd.set_option("display.max_columns", 20)
+    pd.set_option("display.max_rows", 20)
+    pd.set_option('display.width', 200)
+    pd.set_option("display.expand_frame_repr", False)
+    pd.options.mode.chained_assignment = None
+    pd.set_option("display.float_format", lambda x: "%.3f" % x)
+
+
+pd_options()
